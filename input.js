9d82714c537060322d231c315e944b41e3597d5f
Qais Nizami <qais.nizami@gmail.com>
2023-08-25 15:43:16 -0400

    Implemented input.js to separate user input into new module.

---
+++ input.js
@@ -0,0 +1,37 @@
+// input.js
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Register the event listener for "data" event
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    // Ctrl + C was pressed
+    process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  }
+};
+
+module.exports = {
+  setupInput,
+  handleUserInput,
+};
--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 const { connect } = require('./client');
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    // Ctrl + C was pressed
-    process.exit();
-  }
-  // Add more key handlers if needed
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // Register the event listener for "data" event
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const { setupInput, handleUserInput } = require('./input');
 
 console.log("Connecting ...");
-const connection = connect();
-const stdin = setupInput();
+const connection = connect(); // Get the connection object
+setupInput(connection); // Pass the connection object to setupInput
